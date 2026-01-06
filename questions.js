693e5abc6ae0594ce86badf64db85971a9bf5c11
malabaal25 <malabaal@gmail.com>
2026-01-05 21:10:33 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,20 @@
+const QUESTIONS = [
+  {
+    c: "Português",
+    q: "Qual a forma correta de grafia?",
+    o: ["Auto-estima", "Autoestima", "Autooestima", "Auto estima"],
+    r: 1
+  },
+  {
+    c: "Informática",
+    q: "No Windows, qual atalho fecha a janela ativa?",
+    o: ["Alt + F4", "Ctrl + C", "Win + L", "Alt + Tab"],
+    r: 0
+  },
+  {
+    c: "Lei 8.112",
+    q: "O dia 28 de outubro é consagrado ao:",
+    o: ["Professor", "Servidor Público", "Estudante", "Reitor"],
+    r: 1
+  }
+];
